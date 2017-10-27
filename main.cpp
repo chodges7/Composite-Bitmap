bab02330413768bf4f55705ddb8182b813a904a5
chodges7 <cdhodges5@gmail.com>
2017-10-27 12:10:58 -0700

    added 'done' function to quit file entry

--- main.cpp
+++ main.cpp
@@ -6,32 +6,38 @@
 using namespace std;
 
 //Prototypes for functions:
-Bitmap userInput();
+Bitmap userInput(string);
 vector <vector <Pixel> > convertImage(Bitmap);
+void saveImage(int, vector <vector <Pixel> >);
 
 int main(){
         //Asks user for 10 images
         for (int index = 0; index <= 10; index++){
                 vector <vector <Pixel> > bmp;
+                string name;
 
-                bmp = convertImage(userInput());
-                cout << "File " << index + 1 << " entered.\n";
+                cout << "Enter file name: ";
+                cin >> name;
+                if (name == "DONE" || name == "done"){
+                        index = 10;
+                }
+                else{
+                        bmp = convertImage(userInput(name));
+                        cout << "File " << index + 1 << " entered.\n";
+
+                        saveImage(index, bmp);
+                }
         }
 }
 
 
-Bitmap userInput(){
-        string name;
+Bitmap userInput(string name){
         Bitmap image;
-           
-        //Asks user for file name
-        cout << "Enter file name: ";
-        cin >> name;
 
         //Checks to see if file is correct format
         image.open(name);
         bool validBmp = image.isImage();
-        
+
         if (validBmp == true){
                 return image;
         }
@@ -44,4 +50,42 @@ vector <vector <Pixel> > convertImage(Bitmap image){
         vector <vector <Pixel> > bmp;
         bmp = image.toPixelMatrix();
         return bmp;
+}
+
+void saveImage(int index, vector <vector <Pixel> > bmp){
+        Bitmap image;
+        string name;
+
+        if (index == 0){
+                name = "1.bmp";
+        }
+        else if (index == 1){
+                name = "2.bmp";
+        }
+        else if (index == 2){
+                name = "3.bmp";
+        }
+        else if (index == 3){
+                name = "4.bmp";
+        }
+        else if (index == 4){
+                name = "5.bmp";
+        }
+        else if (index == 5){
+                name = "6.bmp";
+        }
+        else if (index == 6){
+                name = "7.bmp";
+        }
+        else if (index == 7){
+                name = "8.bmp";
+        }
+        else if (index == 8){
+                name = "9.bmp";
+        }
+        else if (index == 9){
+                name = "10.bmp";
+        }
+        image.fromPixelMatrix(bmp);
+        image.save(name);
 }
