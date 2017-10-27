5f5f73535540d3b8f0991490c713d4b21d75ff52
chodges7 <cdhodges5@gmail.com>
2017-10-27 23:58:08 +0000

    testing things

--- main.cpp
+++ main.cpp
@@ -8,97 +8,117 @@ using namespace std;
 //Prototypes for functions:
 Bitmap userInput(string, int &index);
 vector <vector <Pixel> > convertImage(Bitmap);
-void saveImage(int, vector <vector <Pixel> >);
+void saveImage(string &name, vector <vector <Pixel> >, int);
+Bitmap combineImages(int);
+
+string firstimage;
+string secondimage;
+string thirdimage;
+string fourthimage;
+string fifthimage;
+string sixthimage;
+string seventhimage;
+string eigthimage;
+string ninthimage;
+string tenthimage;
 
 int main(){
 
-        int howMany = 0;
-        //Asks user for 10 images
-        for (int index = 0; index < 10; index++){
-                vector <vector <Pixel> > bmp;
-                string name;
+	int howMany = 0;
+	//Asks user for 10 images
+	for (int index = 0; index < 10; index++){
+		vector <vector <Pixel> > bmp;
+		string name;
+		cout << "Enter file name: ";
+		cin >> name;
 
-                cout << "Enter file name: ";
-                cin >> name;
-                if (name == "DONE" || name == "done"){
-                        index = 10;
-                }
-                else{
-                        bmp = convertImage(userInput(name, index));
-                        saveImage(index, bmp);
-                        howMany++;
-                }
-        }
-        if (howMany == 0){
-                cout << "No files were entered. Please try again.\n";
-        }
-        /*
-        if (howMany > 0){
-                for (int index = 0; index < howMany; index++){
-                vector <vector <Pixel> > bmp;
-                string name;
-                bmp = convertImage();
-                }
-        }
-        */
+		if (name == "DONE" || name == "done"){
+			index = 10;
+		}
+		else{
+			bmp = convertImage(userInput(name, index));
+			saveImage(name, bmp, index);
+			howMany++;
+		}
+	}
+	if (howMany == 0){
+		cout << "No files were entered. Please try again.\n";
+	}	
+	if (howMany > 0){
+		for (int imageNum = 0; imageNum < howMany; imageNum++){
+			combineImages(imageNum);
+		}
+	}
 }
 
+Bitmap combineImages(int imageNum){
+	Bitmap image;
+	Bitmap secondImage;
+	vector <vector <Pixel> > bmp;
+	vector <vector <Pixel> > secondbmp;
+	Pixel rgb;
+	Pixel secondrgb;
+	if (imageNum = 0){
+		image.open(firstimage);
+		secondImage(secondImage)
+	}
+}
 
 Bitmap userInput(string name, int &index){
-        Bitmap image;
-        //Checks to see if file is correct format
-        image.open(name);
-        bool validBmp = image.isImage();
+	Bitmap image;
+	//Checks to see if file is correct format
+	image.open(name);
+	bool validBmp = image.isImage();
 
-        if (validBmp == true){
-                cout << "File " << index + 1 << " entered.\n";
-                return image;
-        }
-        if (validBmp == false){
-                index--;
-        }
+	if (validBmp == true){
+		cout << "File " << index + 1 << " entered.\n";
+		return image;
+	}
+	if (validBmp == false){
+		index--;
+	}
 }
 
 vector <vector <Pixel> > convertImage(Bitmap image){
-        vector <vector <Pixel> > bmp;
-        bmp = image.toPixelMatrix();
-        return bmp;
+	vector <vector <Pixel> > bmp;
+	bmp = image.toPixelMatrix();
+	return bmp;
 }
 
-void saveImage(int index, vector <vector <Pixel> > bmp){
-        Bitmap image;
-        string name;
+void saveImage(string &name, vector <vector <Pixel> > bmp, int index){
+	Bitmap image;
 
-        if (index == 0){
-                name = "1.bmp";
-        }
-        else if (index == 1){
-                name = "2.bmp";
-        }
-        else if (index == 2){
-                name = "3.bmp";
-        }
-        else if (index == 3){
-                name = "4.bmp";
-        }
-        else if (index == 4){
-                name = "5.bmp";
-        }
-        else if (index == 5){
-                name = "6.bmp";
-        }
-        else if (index == 6){
-                name = "7.bmp";
-        }
-        else if (index == 7){
-                name = "8.bmp";
-        }
-        else if (index == 8){
-                name = "9.bmp";
-        }
-        else if (index == 9){
-                name = "10.bmp";
-        }
-        image.fromPixelMatrix(bmp);
-        image.save(name);
+	if (index == 0){
+		firstimage = name;
+	}
+	else if (index == 1){
+		secondimage = name;
+	}
+	else if (index == 2){
+		thirdimage = name;
+	}
+	else if (index == 3){
+		fourthimage = name;
+	}
+	else if (index == 4){
+		fifthimage = name;	
+	}
+	else if (index == 5){
+		sixthimage = name;
+	}
+	else if (index == 6){
+		seventhimage = name;
+	}
+	else if (index == 7){
+		eigthimage = name;
+	}
+	else if (index == 8){
+		ninthimage = name;
+	}
+	else if (index == 9){
+		tenthimage = name;
+	}
+
+	image.fromPixelMatrix(bmp);
+	image.save(name);
 }
