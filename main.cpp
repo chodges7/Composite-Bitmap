2f1523eaf9f06294293869527446cd668548b68b
chodges7 <cdhodges5@gmail.com>
2017-10-30 04:16:07 +0000

    Almost done, except it fails and aborts after entering file

--- main.cpp
+++ main.cpp
@@ -10,7 +10,8 @@ Bitmap userInput(string, int &howMany);
 vector <vector <Pixel> > convertImage(Bitmap);
 void saveImage(vector <vector <Pixel> >);
 Bitmap combineImages(int);
-vector <vector <Pixel> > applyToCavas(vector< vector <Pixel> >, vector <vector <Pixel> >);
+vector <vector <Pixel> > applyToCanvas(vector< vector <Pixel> >, vector <vector <Pixel> >);
+bool sameSize = true;
 
 int main(){
 
@@ -29,12 +30,12 @@ int main(){
 		if (name == "DONE" || name == "done"){
 			done = true;
 		}
-		else if (howMany == 10){
+		else if (howMany == 9){
 			done = true;
 		}
 		else{
 			bmp = convertImage(userInput(name, howMany));
-			
+
 			//If it's the first time through, it resizes canvas
 			if (howMany == 0){
 				int rows = bmp.size();
@@ -46,32 +47,45 @@ int main(){
 			}
 
 			canvas = applyToCanvas(canvas, bmp);
+			if (sameSize == false){
+				//If during the applyToCanvas stage, the files were different sizes
+				//then it stops the program.
+				cout << "Please try again\n";				
+				return 0;
+			}
+			cout << "File " << howMany + 1 << " applied to canvas.\n";
 			howMany++;
 		}
 	}
 	while (done == false);
-saveImage(canvas);
-return 0;
+	cout << "DONE! Please see canvas.bmp file for output.\n";
+	saveImage(canvas);
+	return 0;
 }
 
 vector <vector <Pixel> > applyToCanvas(vector <vector <Pixel> > canvas, vector <vector <Pixel> > bmp){
 	Pixel rgb;
 	Pixel crgb;
 
-	for (int rodex = 0; rodex < canvas.size(); rodex++){
-		for (int coldex = 0; coldex < canvas[rodex].size();coldex++){
-			rgb = bmp[rodex][coldex];
-			crgb = canvas[rodex][coldex];
-			crgb.red = rgb.red + crgb.red;
-			crgb.red = crgb.red / 2;
-			crgb.blue = rgb.blue + crgb.blue;
-			crgb.blue = crgb.blue / 2;
-			crgb.green = rgb.green + crgb.green;
-			crgb.green = crgb.green / 2;
-			canvas[rodex][coldex] = crgb;
-		}
+	if (canvas.size() != bmp.size() && canvas[0].size() != bmp[0].size()){
+		cout << "Some of the .bmp files aren't the right sizes\n";	
 	}
-	return canvas;
+	else{	
+		for (int rodex = 0; rodex < canvas.size(); rodex++){
+			for (int coldex = 0; coldex < canvas[rodex].size();coldex++){
+				rgb = bmp[rodex][coldex];
+				crgb = canvas[rodex][coldex];
+				crgb.red = rgb.red + crgb.red;
+				crgb.red = crgb.red / 2;
+				crgb.blue = rgb.blue + crgb.blue;
+				crgb.blue = crgb.blue / 2;
+				crgb.green = rgb.green + crgb.green;
+				crgb.green = crgb.green / 2;
+				canvas[rodex][coldex] = crgb;
+			}
+		}
+		return canvas;
+	}
 }
 
 Bitmap userInput(string name, int &howMany){
@@ -90,6 +104,7 @@ Bitmap userInput(string name, int &howMany){
 }
 
 vector <vector <Pixel> > convertImage(Bitmap image){
+	//Converts Bitmap file format into Pixel Matrix
 	vector <vector <Pixel> > bmp;
 	bmp = image.toPixelMatrix();
 	return bmp;
